9f67015c4429cb482ea1038cca663adf90fa949b
joaojacinto <joaorsj96@gmail.com>
2023-01-16 10:59:09 +0000

    16-01

--- View/index.php
+++ View/index.php
@@ -20,41 +20,14 @@
                 <li><a href="#"><i class="fas fa-project-diagram"></i>Contacto</a></li>
             </ul> 
             <div class="social_media">
-              <a href="https://www.facebook.com/pinceladasAJ" target=_blank ><img src="C:\xampp\htdocs\webProject\View\icons\facebook.png"><i class="fab fa-facebook-f"></i></a>
-              <a href="https://www.instagram.com/pinceladas.aj/" target=_blank ><img src="C:\xampp\htdocs\webProject\View\icons\instagram.png"><i class="fab fa-facebook-f"></i></a>
-              <a href="https://www.pinterest.pt/alinejacinto134/" target=_blank ><img src="C:\xampp\htdocs\webProject\View\icons\pinterest.png"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://www.facebook.com/pinceladasAJ" target=_blank class="fa fa-facebook"></a>
+              <a href="https://www.instagram.com/pinceladas.aj/" target=_blank class="fa fa-instagram"></a>
+              <a href="https://www.pinterest.pt/alinejacinto134/" target=_blank class="fa fa-pinterest"></a>
           </div>
         </div>
         <div class="main_content">
             <div class="header">Pinceladas</div>  
             <div class="info">
-            <?php
-                include '/xampp/htdocs/webProject/Controller/connect.php';
-                $sql = "SELECT * FROM photos";
-                if ($result = mysqli_query($conn, $sql)) {
-                    if (mysqli_num_rows($result) > 0) {
-                        echo '<table border="1px">';
-                        echo "<tr>";
-                        echo "<th>Preco (EUR)</th>";
-                        echo "<th>Nome</th>";
-                        echo "<th>Imagem</th>";
-                        echo "</tr>";
-                        while ($row = mysqli_fetch_array($result)) {
-                            echo "<tr>";
-                            echo "<td>" . $row['id'] . "</td>";
-                            echo "<td>" . $row['photo_name'] . "</td>";
-                            echo "<td>" . "<img src=".$row['photo_file_name'].' width=500px height="500px">' . "</td>";
-                            echo "</tr>";
-                        }
-                        echo "</table>";
-                        mysqli_free_result($result);
-                    } else {
-                        echo "No records found";
-                    }
-                } else {
-                    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-                }
-            ?>
           </div>
         </div>
     </div> 
--- View/loja.php
+++ View/loja.php
@@ -10,53 +10,31 @@
 <body>
 
     <div class="wrapper">
-        <div class="sidebar">
-            <h2>Pinceladas</h2>
-            <ul>
-                <li><a href="index.php"><i class="fas fa-home"></i>Home</a></li>
-                <li><a href="loja.php"><i class="fas fa-user"></i>Loja</a></li>
-                <li><a href="galeria.php"><i class="fas fa-address-card"></i>Galeria</a></li>
-                <li><a href="#"><i class="fas fa-project-diagram"></i>Contacto</a></li>
-            </ul> 
-            <div class="social_media">
-              <a href="#"><i class="fab fa-facebook-f"></i></a>
-              <a href="#"><i class="fab fa-twitter"></i></a>
-              <a href="#"><i class="fab fa-instagram"></i></a>
-          </div>
-        </div>
-        <div class="main_content">
-            <div class="header">Pinturas a carvao</div>  
-            <div class="info">
-            <?php
-                include '/xampp/htdocs/webProject/Controller/connect.php';
-                $sql = "SELECT * FROM photos";
-                if ($result = mysqli_query($conn, $sql)) {
-                    if (mysqli_num_rows($result) > 0) {
-                        echo '<table border="1px">';
-                        echo "<tr>";
-                        echo "<th>Preco (EUR)</th>";
-                        echo "<th>Nome</th>";
-                        echo "<th>Imagem</th>";
-                        echo "</tr>";
-                        while ($row = mysqli_fetch_array($result)) {
-                            echo "<tr>";
-                            echo "<td>" . $row['id'] . "</td>";
-                            echo "<td>" . $row['photo_name'] . "</td>";
-                            echo "<td>" . "<img src=".$row['photo_file_name'].' width=500px height="500px">' . "</td>";
-                            echo "</tr>";
-                        }
-                        echo "</table>";
-                        mysqli_free_result($result);
-                    } else {
-                        echo "No records found";
-                    }
-                } else {
-                    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-                }
-            ?>
-          </div>
-        </div>
-    </div>
+            <div class="sidebar">
+                <h2>Pinceladas</h2>
+                <ul>
+                    <li><a href="index.php"><i class="fas fa-home"></i>Home</a></li>
+                    <li><a href="loja.php"><i class="fas fa-user"></i>Loja</a></li>
+                    <li><a href="galeria.php"><i class="fas fa-address-card"></i>Galeria</a></li>
+                    <li><a href="#"><i class="fas fa-project-diagram"></i>Contacto</a></li>
+                </ul> 
+                <div class="social_media">
+                <a href="https://www.facebook.com/pinceladasAJ" target=_blank class="fa fa-facebook"></a>
+                <a href="https://www.instagram.com/pinceladas.aj/" target=_blank class="fa fa-instagram"></a>
+                <a href="https://www.pinterest.pt/alinejacinto134/" target=_blank class="fa fa-pinterest"></a>
+            </div>
+            </div>
+            <div class="main_content">
+                <div class="header">LOJA</div>  
+                <div class="info"></div>
+                <div class="info">
+                    <form action="/Controller/upload.php" method="post" enctype="multipart/form-data">Select image to upload:
+                      <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload Image" name="submit">
+                    </form>
+                </div>
+            </div>
+        </div> 
     
 </body>
 </html>
